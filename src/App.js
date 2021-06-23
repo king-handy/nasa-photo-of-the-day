7c16f36565da2d57b98d53fefcca2520be7e2bf1
king-handy <a.king.life@pm.me>
2021-06-22 22:31:47 -0400

    photo render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import "./App.css";
 
 function App() {
+  const [photoOfDay, setPhotoOfDay] = useState([])
+
   useEffect(() => {
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-    .then(({data}) => console.log(data))
+    .then(res => setPhotoOfDay(res.data.url))
   }, [])
 
   return (
     <div className="App">
+      <br></br>
+      <h1>NASA photo of the day!</h1>
+      <img src={photoOfDay} alt='NASA Photo of Day' />
+      
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        To the moon <span role="img" aria-label='go!'>🚀</span>!
       </p>
     </div>
   );
